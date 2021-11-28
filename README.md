be428d55b1f48e45edf53c87cf806ba8e0eee982
Julio Briones <Julio_Briones@epam.com>
2021-11-28 10:46:16 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+#App fromo Teclado Udemy Course: REST APIs with Flask and Python
+
+###Note: Project says student, but it's more related to product items.
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+
+# a model, urls
+class Item(Resource):
+    def get(self, name):
+        """Get an item."""
+        item = next((filter(lambda x: x["name"] == name, items)), None)
+        return {"item": item}, 200 if item is not None else 404
+
+    def post(self, name):
+        """Create a new item."""
+        if next((filter(lambda x: x["name"] == name, items)), None):
+            return {"message": f"An item with name {name} already exists."}, 400
+
+        data = request.get_json()
+        item = {"name": name, "price": data["price"]}
+        items.append(item)
+
+        return item, 201
+
+
+class ItemList(Resource):
+    def get(self):
+        """Return all items."""
+        return {"items": items}
+
+
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+
+app.run(port=5000, debug=True)
