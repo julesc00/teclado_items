99e57a13457760ec47af8b4064b88b9d90ae751f
Julio Briones <Julio_Briones@epam.com>
2021-11-29 06:24:10 -0600

    Added authentication and flask JWT.

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = "julito"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # new endpoint /auth
 
 items = []
 
 
 # a model, urls
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         """Get an item."""
         item = next((filter(lambda x: x["name"] == name, items)), None)
-        return {"item": item}, 200 if item is not None else 404
+        return {"item": item}, 200 if item else 404
 
     def post(self, name):
         """Create a new item."""
